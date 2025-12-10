65977a12f0b0c78bc6f96b40857adb59654f7fca
Margel <margeldestinekrizia@gmail.com>
2025-12-11 03:19:34 +0800

    attendance responsive date

--- src/main/java/com/appdevg5/geeks/GeeksApplication.java
+++ src/main/java/com/appdevg5/geeks/GeeksApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GeeksApplication {
 				
-	public static void main(String[] args) {
-		SpringApplication.run(GeeksApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GeeksApplication.class, args);
+    }
 }
